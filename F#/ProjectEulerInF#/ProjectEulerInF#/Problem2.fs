4b160020ddc16ffb02bdca23959584bf522c9ac1
zchpit <rabiniak.piotr@gmail.com>
2016-01-07 23:33:58 +0100

    problem 2

--- F#/ProjectEulerInF#/ProjectEulerInF#/Problem2.fs
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem2.fs
@@ -3,15 +3,15 @@ open System
 
 module Problem2 = type T = X
 
-
    let rec fib n =
    if n <= 2 then 1
       else
-        let a = fib (n - 1)
-        let b = fib (n - 2)
-
+      let a = fib (n - 1)
+      let b = fib (n - 2)
+      if a > 4000000
+        then 0
+      else
         a + b
-
 
 let solution = [ for i in 1 .. 35 -> fib i] |> List.filter(fun x -> x % 2 = 0) |> List.sum
 
