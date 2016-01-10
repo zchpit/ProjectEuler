c61a7eb3ad827c66c6536cd071673049a5b6d424
zchpit <rabiniak.piotr@gmail.com>
2016-01-10 23:48:21 +0100

    add faster solution to problem2

--- F#/ProjectEulerInF#/ProjectEulerInF#/Problem2.fs
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem2.fs
@@ -3,6 +3,7 @@ open System
 
 module Problem2 = type T = X
 
+//first attempt
    let rec fib n =
    if n <= 2 then 1
       else
@@ -16,5 +17,22 @@ module Problem2 = type T = X
 let solution = [ for i in 1 .. 35 -> fib i] |> List.filter(fun x -> x % 2 = 0) |> List.sum
 
 
+//second attempt, from Tomas Petricek via stackoverflow: http://stackoverflow.com/questions/2845744/generating-fibonacci-series-in-f
 
+let rec fibsRec a b =
+  if a + b < 4000000 then
+    // The current element
+    let current = a + b
+    // Calculate all remaining elements recursively 
+    // using 'b' as 'a' and 'current' as 'b' (in the next iteration)
+    let rest = fibsRec b current  
+    // Return the remaining elements with 'current' appended to the 
+    // front of the resulting list (this constructs new list, 
+    // so there is no mutation here!)
+    current :: rest
+  else 
+    [] // generated all elements - return empty list once we're done
 
+// generate list with 1, 2 and all other larger fibonaccis
+let fibs = 1::2::(fibsRec 1 2)
+let solution2 = fibs |> List.filter(fun x -> x % 2 = 0) |> List.sum
