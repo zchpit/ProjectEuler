27355ac75bb20fd20946462cd5a42a6baace7a29
zchpit <rabiniak.piotr@gmail.com>
2016-01-17 22:45:33 +0100

    solution to problem 7

---
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem7.fs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Application
+open System
+
+ let isPrime n =
+    let sqrt' = (float >> sqrt >> float) n // square root of integer
+    [ 2.0 .. sqrt' ] // all numbers from 2 to sqrt'
+    |> List.forall (fun x -> n % x <> 0.0) // no divisors
+ 
+ let allPrimes =
+   let rec allPrimes' n =
+     seq { // sequences are lazy, so we can make them infinite
+       if isPrime n then
+         yield n
+       yield! allPrimes' (n+1.0) // recursing
+     }
+   allPrimes' 2.0 // starting from 2
+ 
+ let ourPrime = allPrimes |> Seq.take 10001  |> Seq.last 
