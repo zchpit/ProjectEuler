6876e44a5e0fb7bf05168f8a6999c105b339b125
zchpit <rabiniak.piotr@gmail.com>
2016-01-17 19:36:49 +0100

    solution for problem 4

--- F#/ProjectEulerInF#/ProjectEulerInF#/Problem4.fs
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem4.fs
@@ -10,12 +10,15 @@ let isPalindrome (s: string) =
    let arr = s.ToCharArray()
    arr = Array.rev arr
 
-let min3digit = 111
-let max3digit = 999
-let minValue = min3digit * min3digit;
-let maxValue = max3digit * max3digit;
-
 let palidrome = isPalindrome "aba"
 let notPalidrome = isPalindrome "nie"
 
-let solution = [ minValue .. maxValue ] |> List.map string |>  List.filter(fun x -> isPalindrome x) |> List.map int |> List.max
+let min3digit = 111
+let max3digit = 999
+
+let xList = [ 111 .. 999 ]
+let yList = [ 111 .. 999 ]
+
+let list = xList |> List.collect (fun x -> yList |> List.map (fun y -> x * y))
+
+let solution = list |> List.map string |>  List.filter(fun x -> isPalindrome x) |> List.map int |> List.max
