0f2de9df478cca401c9f122cbb98acfbfee2fea2
zchpit <rabiniak.piotr@gmail.com>
2016-01-17 16:38:36 +0100

    correct answare for problem 3

--- F#/ProjectEulerInF#/ProjectEulerInF#/Problem3.fs
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem3.fs
@@ -2,22 +2,21 @@
 open System
 
  let isPrime n =
-    let sqrt' = (float >> sqrt >> int) n // square root of integer
-    [ 2 .. sqrt' ] // all numbers from 2 to sqrt'
-    |> List.forall (fun x -> n % x <> 0) // no divisors
+    let sqrt' = (float >> sqrt >> float) n // square root of integer
+    [ 2.0 .. sqrt' ] // all numbers from 2 to sqrt'
+    |> List.forall (fun x -> n % x <> 0.0) // no divisors
  
  let allPrimes =
    let rec allPrimes' n =
      seq { // sequences are lazy, so we can make them infinite
        if isPrime n then
          yield n
-       yield! allPrimes' (n+1) // recursing
+       yield! allPrimes' (n+1.0) // recursing
      }
-   allPrimes' 2 // starting from 2
+   allPrimes' 2.0 // starting from 2
  
- let primes = allPrimes |> Seq.take 200  |> List.ofSeq 
+ let primes = allPrimes |> Seq.take 2000  |> List.ofSeq 
 
-
- let solution = primes |> List.filter(fun x -> 13195 % x = 0)
+ let solution = primes |> List.filter(fun x -> 600851475143.0 % x = 0.0) |> List.max
 
 
