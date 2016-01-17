a345a4bf8fa65319a67c6e6f3facd611fb6ea189
zchpit <rabiniak.piotr@gmail.com>
2016-01-17 20:47:23 +0100

    Problem 5 solved

---
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem5.fs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Application2
+open System
+
+module Problem5 = type T = X
+
+
+let rec isDivided (n: int, m: int) =
+     if m = 1 then true
+     else
+        if
+           n % m <> 0 then false
+        else
+           isDivided (n, m-1)
+
+let check = isDivided (2520,10)
+
+
+let solution = seq {5 .. 500000000} |> Seq.filter(fun x -> isDivided(x,20)) |> Seq.min
+
+
