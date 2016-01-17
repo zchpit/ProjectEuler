7818a471313cd435d3f403f8753e26d585fa34dc
zchpit <rabiniak.piotr@gmail.com>
2016-01-17 19:24:49 +0100

    almost

---
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem4.fs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Application
+open System
+
+type Problem4() = "Problem4"
+
+
+let isPalindrome (s: string) =
+   let arr = s.ToCharArray()
+   arr = Array.rev arr
+
+let min3digit = 111
+let max3digit = 999
+let minValue = min3digit * min3digit;
+let maxValue = max3digit * max3digit;
+
+let palidrome = isPalindrome "aba"
+let notPalidrome = isPalindrome "nie"
+
+let solution = [ minValue .. maxValue ] |> List.map string |>  List.filter(fun x -> isPalindrome x) |> List.map int |> List.max
