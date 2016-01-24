219fe1f779d4e5a728fdd9c3d55fa17c9df07d9f
zchpit <rabiniak.piotr@gmail.com>
2016-01-25 00:10:46 +0100

    Problem 9 in progress

---
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem9.fs
@@ -0,0 +1,11 @@
+﻿namespace Application
+open System
+
+let xList = [ for i in 1 .. 1000 -> i*i ]
+let yList = [ for i in 1 .. 1000 -> i*i ]
+
+let zList = xList |> List.collect (fun x -> yList |> List.map (fun y -> x + y))
+
+let filter = set xList
+let cont = zList |> List.filter (fun a -> Set.contains a filter) |> List.distinct
+
