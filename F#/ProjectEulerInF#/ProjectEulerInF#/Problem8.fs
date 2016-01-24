4058870e6df705e97fb189de96dafcb87c07748f
zchpit <rabiniak.piotr@gmail.com>
2016-01-24 21:01:31 +0100

    problem 8, work in progress

--- F#/ProjectEulerInF#/ProjectEulerInF#/Problem8.fs
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem8.fs
@@ -1,8 +1,9 @@
 ﻿namespace Application
 open System
 
+module Problem8 = type T = X
 
-
+let string = "73167176531330624919225119674426574742355349194934"
 
 let multiply list =
    let rec loop list acc =
@@ -10,12 +11,22 @@ let multiply list =
        | head :: tail -> loop tail (acc * head)
        | [] -> acc
    loop list 1.0
- 
-let list1 = [1.0 .. 3.0]
-let test = multiply list1
 
-
-let string = "73167176531330624919225119674426574742355349194934"
-let num2 = string |> Seq.take 5 |> Seq.map Char.GetNumericValue |> Seq.toList
+let num2 = string |> Seq.skip 5 |> Seq.take 5 |> Seq.map Char.GetNumericValue |> Seq.toList
 let test1 = multiply num2
+
+let rec getMaxSeq (strAll:string, minNum:int, takeNum:int, accMax:float) =
+    let current = strAll |> Seq.skip minNum |> Seq.take takeNum |> Seq.map Char.GetNumericValue |> Seq.toList
+    let currentVal = multiply current
+    if minNum > 20 then accMax
+    else
+       if
+          currentVal > accMax then
+          accMax = currentVal;
+          getMaxSeq(strAll,minNum+1,takeNum,accMax)
+       else
+          getMaxSeq(strAll,minNum+1,takeNum,accMax)
+
+let test = getMaxSeq(string,0,5,1.0)
+
 
