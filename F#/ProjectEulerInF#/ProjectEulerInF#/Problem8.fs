ba752ad4c746fad48d2eab5f804499f16af63f65
zchpit <rabiniak.piotr@gmail.com>
2016-01-24 19:42:19 +0100

    problem 8, work in progress

---
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem8.fs
@@ -0,0 +1,21 @@
+﻿namespace Application
+open System
+
+
+
+
+let multiply list =
+   let rec loop list acc =
+       match list with
+       | head :: tail -> loop tail (acc * head)
+       | [] -> acc
+   loop list 1.0
+ 
+let list1 = [1.0 .. 3.0]
+let test = multiply list1
+
+
+let string = "73167176531330624919225119674426574742355349194934"
+let num2 = string |> Seq.take 5 |> Seq.map Char.GetNumericValue |> Seq.toList
+let test1 = multiply num2
+
