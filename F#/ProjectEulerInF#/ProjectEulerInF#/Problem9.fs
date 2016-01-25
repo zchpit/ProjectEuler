e2c204ca48df80da0793d61f64e1b93a95a3c5a8
zchpit <rabiniak.piotr@gmail.com>
2016-01-25 01:01:46 +0100

    still work on problem 9

--- F#/ProjectEulerInF#/ProjectEulerInF#/Problem9.fs
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem9.fs
@@ -6,9 +6,12 @@ let filterWithSet set lst = lst |> List.filter (fun (_, elem) -> Set.contains el
 let xList = [ for i in 1 .. 1000 -> i*i ] |> List.distinct
 let yList = [ for i in 1 .. 1000 -> i,i*i ] |> List.distinct
 let zList = xList |> List.collect (fun x -> xList |> List.map (fun y -> x + y)) |> List.distinct |> List.sort
+let xxList = xList |> List.map (fun x -> xList |> List.map (fun y -> x + y),x) |> List.distinct
+
 
 let filter = set xList
 let cont = zList |> List.filter (fun a -> Set.contains a filter) |> List.distinct |> Set.ofList
 
-let tList = [1000] |> Set.ofList
-let abc = filterWithSet cont yList |> List.filter(fun (a,b)-> a = 1000) |> List.map(fun(a,b) -> b)
+let toAddList = filterWithSet cont yList //|> List.filter(fun (a,b)-> b = 1000000) |> List.map(fun(a,b) -> a)
+
+//let sol1 = xxList |> List.filter(fun (a,b) -> b = 1000) |> List.map(fun(a,b) -> b)
