599c936385286127d22711488d266359e41293b8
zchpit <rabiniak.piotr@gmail.com>
2016-01-26 20:45:55 +0100

    problem 10 solved

---
+++ F#/ProjectEulerInF#/ProjectEulerInF#/Problem10.fs
@@ -0,0 +1,18 @@
+﻿
+ let isPrime n =
+    let sqrt' = (float >> sqrt >> float) n // square root of integer
+    [ 2.0 .. sqrt' ] // all numbers from 2 to sqrt'
+    |> List.forall (fun x -> n % x <> 0.0) // no divisors
+ 
+ let allPrimes =
+   let rec allPrimes' n =
+     seq { 
+        if n < 2000000.0 then
+            if isPrime n then
+              yield n
+            yield! allPrimes' (n+1.0) // recursing
+     }
+   allPrimes' 2.0 // starting from 2
+ 
+ let primes = allPrimes |> List.ofSeq 
+ let solution = primes |> List.sum
