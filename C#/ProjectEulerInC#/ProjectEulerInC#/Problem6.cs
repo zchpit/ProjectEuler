58438f04518c469df5b11a4fb057dbfbb2541e3b
zchpit <rabiniak.piotr@gmail.com>
2016-01-31 20:18:22 +0100

    solution to problem 6

---
+++ C#/ProjectEulerInC#/ProjectEulerInC#/Problem6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ProjectEulerInC
+{
+	public class Problem6
+	{
+		public Problem6 ()
+		{
+		}
+		public static int GetSolution()
+		{
+			int sumOfSquare = 0;
+			int squareOfSum = 0;
+
+			for (int i = 1; i <= 100; i++) {
+				int square = i * i;
+				sumOfSquare += square;
+				squareOfSum += i;
+			}
+			squareOfSum = squareOfSum * squareOfSum;
+
+			int solution = squareOfSum - sumOfSquare;
+			return solution;
+		}
+	}
+}
+
--- C#/ProjectEulerInC#/ProjectEulerInC#/Program.cs
+++ C#/ProjectEulerInC#/ProjectEulerInC#/Program.cs
@@ -7,7 +7,7 @@ namespace ProjectEulerInC
 		public static void Main (string[] args)
 		{
 			//Problem4.SpeedTest ();
-			var solution = Problem5.GetSolution ();
+			var solution = Problem6.GetSolution ();
 			Console.WriteLine (solution);
 			Console.ReadKey ();
 		}
