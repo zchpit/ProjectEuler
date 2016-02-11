c2439ba4b70cc273c6eb063d7e71024737736d01
zchpit <rabiniak.piotr@gmail.com>
2016-02-11 22:57:19 +0100

    problem25 solved in c#

--- C#/ProjectEulerInC#/ProjectEulerInC#/Program.cs
+++ C#/ProjectEulerInC#/ProjectEulerInC#/Program.cs
@@ -7,7 +7,7 @@ namespace ProjectEulerInC
 		public static void Main (string[] args)
 		{
 			//Problem4.SpeedTest ();
-			Console.WriteLine (Problem24.GetSolution ());
+			Console.WriteLine (Problem25.GetSolution ());
 			Console.ReadKey ();
 		}
 	}
